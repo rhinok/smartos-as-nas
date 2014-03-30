3572786e80b61d33cc10748c013bd3dceb70c9c4
Paul Orlando <k.rhino@yahoo.com>
2014-03-30 22:09:56 +0300

    changed to ups-nut.sh

--- opt/custom/share/global/system/cron/ups-status.sh
+++ opt/custom/share/global/system/cron/ups-status.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
-status=`upsc mustek@localhost | wc -l | awk '{ print $1 }'`
+status=`/opt/local/bin/upsc mustek@localhost | wc -l | awk '{ print $1 }'`
 if [ ${status} -lt 3 ]
 then
-  svcadm disable svc:/site/ups-nut
+  #svcadm disable svc:/site/ups-nut
+  /opt/custom/share/svc/ups-nut.sh stop
   sleep 3
-  svcadm enable svc:/site/ups-nut
+  #svcadm enable svc:/site/ups-nut
+  /opt/custom/share/svc/ups-nut.sh start
 fi
